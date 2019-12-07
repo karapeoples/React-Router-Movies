726ed6c6ca397f5a9e8595d1a3065543d1b692bc
karapeoples <52808542+karapeoples@users.noreply.github.com>
2019-12-07 08:19:58 -0600

    MVP Done? Except for Unclear Error Because: [MVP is Done} [Place to start with stretch] [New Code to Commit]
    This commit:
    [does show that I have met all goals even with an unclear error]
    [does work just cannot check functionality of home button]

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
-
+import {Route} from 'react-router-dom'
 import SavedList from './Movies/SavedList';
+import MoviesList from './Movies/MovieList';
+import Movie from './Movies/Movie'
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -12,7 +14,8 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route exact path='/' component={MoviesList}/>
+      <Route path='/movies/:id' component={Movie}/>
     </div>
   );
 };
